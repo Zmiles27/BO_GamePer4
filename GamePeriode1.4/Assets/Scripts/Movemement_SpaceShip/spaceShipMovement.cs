204e06055a828cae5d7050dd89cfbc0af162294b
unknown <34804@ma-web.nl>
2023-05-11 15:06:32 +0200

    autoFlying

--- GamePeriode1.4/Assets/Scripts/Movemement_SpaceShip/spaceShipMovement.cs
+++ GamePeriode1.4/Assets/Scripts/Movemement_SpaceShip/spaceShipMovement.cs
@@ -15,21 +15,6 @@ public class spaceShipMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            this.transform.Translate(speed * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            this.transform.Translate(0, 0, speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            this.transform.Translate(-speed * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            this.transform.Translate(0, 0, -speed * Time.deltaTime);
-        }
+        this.transform.Translate(speed * Time.deltaTime, 0, 0);
     }
 }
