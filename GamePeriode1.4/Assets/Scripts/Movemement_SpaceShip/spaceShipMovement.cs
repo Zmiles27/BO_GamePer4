f318d74e0e78883d578e7249ac359b90ac5432b3
unknown <34804@ma-web.nl>
2023-05-11 12:02:49 +0200

    fixing ship movement

--- GamePeriode1.4/Assets/Scripts/Movemement_SpaceShip/spaceShipMovement.cs
+++ GamePeriode1.4/Assets/Scripts/Movemement_SpaceShip/spaceShipMovement.cs
@@ -5,23 +5,14 @@ using UnityEngine;
 
 public class spaceShipMovement : MonoBehaviour
 {
-    public float rotSpeed = 50;
     public float speed = 2;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(0, rotSpeed * Time.deltaTime, 0);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(0, -rotSpeed * Time.deltaTime, 0);
-        }
         if (Input.GetKey(KeyCode.Space))
         {
-            transform.Translate(0, 0, -speed * Time.deltaTime);
+            transform.Translate(0, 0, speed * Time.deltaTime);
         }
     }
 }
